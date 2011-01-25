0d3e5bad26b2f13acbbb3af7cd6c6f649ab715bb
Johannes Janosovits <johannes.janosovits@gmail.com>
2011-01-25 15:30:35 +0100

    clock_widget shows time, some config stuff

--- lab/clock_widget.py
+++ lab/clock_widget.py
@@ -4,19 +4,22 @@ import time
 
 class ClockWidget(QFrame):
 
-    def __init__(self, parent):
-        QFrame.__init__(self)
+	def __init__(self, parent):
+		QFrame.__init__(self)
 
-        # It is important to subclass any QtGui base class and apply the
-        # stylesheet to the subclass, otherwise the stylesheet will apply
-        # to any instance of the QtGui base class!
-        self.setStyleSheet("""ClockWidget {
-                border: 2px solid blue;
-                border-radius: 4px;
-                }""")
-        self.resize(300,400)
-        self.label = QLabel(self)
-        self.label.setGeometry(QRect(10, 10, 200, 300))
-        self.label.setText("ahsdfjhasdjkhflashdfhasdjfhajklshdasldhkfjashdfjklhaskldjf")
+		# It is important to subclass any QtGui base class and apply the
+		# stylesheet to the subclass, otherwise the stylesheet will apply
+		# to any instance of the QtGui base class!
+		self.setStyleSheet("""ClockWidget {
+				border: 2px solid blue;
+				border-radius: 4px;
+				}""")
+		self.resize(300,400)
+		self.label = QLabel(self)
+		self.label.setGeometry(QRect(10, 10, 200, 300))
+		self.timer = QTimer()
+		self.connect(self.timer, SIGNAL("timeout()"), self.update_clock)
+		self.timer.start(1000)
 
-        
+	def update_clock(self):
+		self.label.setText(time.strftime("%c", time.localtime()))
--- lab/weather_test_2.py
+++ lab/weather_test_2.py
@@ -21,6 +21,31 @@ MAIN_WIN_WIDTH = 600
 LOCATION_CODE = "Karlsruhe"
 
 
+
+class ConfigParser(object):
+	
+	def __init__(self, config_dir=None):
+		self.config_dir = config_dir
+
+	def setup_config(self, conffile_name):
+		
+		# The directories follow the freedesktop.org standards, except that
+		# custom applets go to XDG_CONFIG_HOME/dsotd/applets
+		if not self.config_dir:
+			if os.environ.has_key("XDG_CONFIG_HOME"):
+				self.config_dir = os.path.join(os.environ["XDG_CONFIG_HOME"], "dsotd")
+			else:
+				self.config_dir = os.path.join(os.environ["HOME"], ".config/dsotd")
+		if not os.access(os.path.join(self.config_dir, conffile_name), os.R_OK):
+			raise IOError("No configuration file found!")
+		
+		conffile = open(os.path.join(self.config_dir, CONFIG_FILE))
+		self.config = json.load(conffile)
+		return self.config
+
+
+
+
 class DataThread(QObject):
 	
 	def __init__(self, parent=None):
@@ -33,52 +58,52 @@ class DataThread(QObject):
 
 
 class WindowThread(QThread):
-    
-    def __init__(self, parent=None):
-        QThread.__init__(self, parent)
-        self.main_window = MainWindow()
-    
-    def run(self):
-        pass
+	
+	def __init__(self, parent=None):
+		QThread.__init__(self, parent)
+		self.main_window = MainWindow()
+	
+	def run(self):
+		pass
 
 class MainWindow(QWidget):
-    def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
-        self.setWindowFlags(Qt.Dialog)
-        self.weather_widget = WeatherWidget(self)
-        self.clock_widget = ClockWidget(self)
-        hbox = QHBoxLayout()
-        hbox.addWidget(self.clock_widget)
-        hbox.addWidget(self.weather_widget)
-        vbox = QVBoxLayout()
-        #vbox.addStretch(1)
-        vbox.addLayout(hbox)
+	def __init__(self, parent=None):
+		QWidget.__init__(self, parent)
+		self.setWindowFlags(Qt.Dialog)
+		self.weather_widget = WeatherWidget(self)
+		self.clock_widget = ClockWidget(self)
+		hbox = QHBoxLayout()
+		hbox.addWidget(self.clock_widget)
+		hbox.addWidget(self.weather_widget)
+		vbox = QVBoxLayout()
+		#vbox.addStretch(1)
+		vbox.addLayout(hbox)
 
-        self.setLayout(vbox)
-        self.resize(MAIN_WIN_WIDTH, MAIN_WIN_HEIGHT)
-        self.setFocus()
+		self.setLayout(vbox)
+		self.resize(MAIN_WIN_WIDTH, MAIN_WIN_HEIGHT)
+		self.setFocus()
 
-    def event(self, event):
+	def event(self, event):
 #        if (event.type()==QEvent.KeyRelease) and ((event.key() == Qt.Key_Super_L)
 #                or (event.key() == Qt.Key_T)) and event.isAutoRepeat() == False:
-        if (event.type() == QEvent.KeyRelease and (event.key() == Qt.Key_Super_L
-                or event.key() == Qt.Key_T) and event.isAutoRepeat() != True):
-        #if event.type() == QEvent.KeyPress or event.type() == QEvent.KeyRelease:
-            print "event type: " + str(event.type())
-            print "event key: " + str(event.key())
-            print "is autorepeat: " + str(event.isAutoRepeat())
-            #self.emit(SIGNAL("close_pressed"))
-            self.hide()
-            return True
+		if (event.type() == QEvent.KeyRelease and (event.key() == Qt.Key_Super_L
+				or event.key() == Qt.Key_T) and event.isAutoRepeat() != True):
+		#if event.type() == QEvent.KeyPress or event.type() == QEvent.KeyRelease:
+			print "event type: " + str(event.type())
+			print "event key: " + str(event.key())
+			print "is autorepeat: " + str(event.isAutoRepeat())
+			#self.emit(SIGNAL("close_pressed"))
+			self.hide()
+			return True
 
-        return QWidget.event(self, event)
+		return QWidget.event(self, event)
 
 def main():
-    dbus.mainloop.qt.DBusQtMainLoop(set_as_default = True)
-    app = QApplication(sys.argv)
-    main_win = DataThread()
-    db = DBusInterface(main_win)
-    app.exec_()
-    
+	dbus.mainloop.qt.DBusQtMainLoop(set_as_default = True)
+	app = QApplication(sys.argv)
+	main_win = DataThread()
+	db = DBusInterface(main_win)
+	app.exec_()
+	
 if __name__ == "__main__":
-    main()
+	main()
